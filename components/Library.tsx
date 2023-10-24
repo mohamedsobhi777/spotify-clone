b20849aa1040dc1e2c03ae8eb2595b5650e5be0f
msmhmorsi <msmhmorsi@connect.ust.hk>
2023-10-24 18:45:51 +0800

    finished player

--- components/Library.tsx
+++ components/Library.tsx
@@ -4,11 +4,20 @@ import { AiOutlinePlus } from 'react-icons/ai'
 import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
+import { Song } from '@/types';
+import MediaItem from './MediaItem';
+import useOnPlay from '@/hooks/useOnPlay';
 
-const Library = () => {
+interface LibraryProps {
+    songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
+
+    const onPlay = useOnPlay(songs);
 
     const onClick = () => {
         if (!user) {
@@ -29,7 +38,15 @@ const Library = () => {
                 <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" />
             </div>
             <div className='flex flex-col gap-y-2 mt-4 px-3'>
-                List of Songs!
+                {
+                    songs.map((item) => (
+                        <MediaItem
+                            onClick={(id: string) => onPlay(id)}
+                            key={item.id}
+                            data={item}
+                        />
+                    ))
+                }
             </div>
         </div>
     )
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+        domains: ["dcycbcxsrteftediyert.supabase.co"],
+    },
+};
+
+module.exports = nextConfig;
